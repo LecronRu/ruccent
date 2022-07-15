d14c858c55694555f5da96822b72fce8fbd6d438
LecronRu <LecronRu@gmail.com>
2022-07-15 18:44:39 +0300

    Initial project

---
+++ README.md
@@ -0,0 +1,27 @@
+﻿## Simple word accent setter.
+
+Pure Python, no dependencies. Based on ngram model.
+
+Trained for the Russian language, on a corpus of ~1000000 word forms, taking into account their frequency in real 
+texts.  Accuracy ~97-98%. Speed ~10000 words per second.
+
+```Python
+from ruccent import Accent
+
+accenter = Accent()
+accenter.predict('пробойник')  # -> 4
+```
+
+You can train the model yourself for your language or corpus.
+```Python
+from ruccent import Accent, AccentTrain
+
+# iterable of tuple (accent position, weight/frequency, word)
+dataset = [(3, 10, 'begin'), (1, 1, 'happy')]
+train = AccentTrain()
+train.fit(dataset, ngram=(3,7))
+train.save('path/yourself.model')
+
+accenter = Accent('path/yourself.model')
+accenter.predict('begin')
+```
---
+++ setup.py
@@ -0,0 +1,39 @@
+﻿from setuptools import setup, find_packages
+
+with open('README.md') as file:
+    description = file.read()
+
+
+with open('requirements.txt') as file:
+    requirements = [_.strip() for _ in file]
+
+
+setup(
+    name='ruccent',
+    version='1.0.1',
+
+    description='Pure Python, no dependencies, simple word accent setter, based on ngram model.',
+    long_description=description,
+    long_description_content_type='text/markdown',
+
+    url='https://github.com/lecronru/ruccent',
+    author='Lecron',
+    author_email='lecronru@gmail.com',
+    license='MIT License',
+
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.10',
+    ],
+    keywords='natural language processing, russian',
+
+#    packages=find_packages(),
+    packages=['ruccent'],
+    package_data={
+        'ruccent': [
+            '*.mdl',
+        ]
+    },
+    setup_requires=['wheel'],
+    install_requires=requirements,
+)
